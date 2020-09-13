2ebb640f9270a82c33a3cfd2827ef5a44a6030d3
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-09-13 19:21:38 +0300

    2. Scanner и чтение числа из консоли. [#352426] - рефакторинг

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,33 +4,29 @@ import java.util.Scanner;
 
 public class Matches {
 	private static int size = 11;
-	private static boolean rsl = true;
-	private static int winner = 1;
+	private static int player = 1;
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		while (rsl) {
-			if (!(takeUser(1, input))) break;
-			if (!(takeUser(2, input))) break;
+		boolean user = false;
+		while (size != 0) {
+			user = !user;
+			player = user ? 1 : 2;
+			takeUser(player, input);
+			System.out.println("Осталось " + size + " спичек");
 		}
-		System.out.println("Игрок " + winner + " WIN!");
+		System.out.println("Игрок " + player + " WIN!");
 	}
 
-	private static boolean takeUser(int numberUser, Scanner input) {
+	private static void takeUser(int numberUser, Scanner input) {
 		System.out.println("Игрок " + numberUser + " возьмите от 1 до 3 спичек");
-		int player = Integer.valueOf(input.nextLine());
-		if (player > 3 || player < 0) {
+		int playerInput = Integer.valueOf(input.nextLine());
+		if (playerInput > 3 || playerInput < 0 || playerInput > size) {
 			System.out.println("Игрок " + numberUser
-					+ " взял не правильное количество спичек: " + player);
+					+ " взял не правильное количество спичек: " + playerInput);
 			System.out.println("Возврат спичек и пропуск хода");
 		} else {
-			size -= player;
-			System.out.println("Осталось " + size + " спичек");
-			if (size == 0) {
-				winner = numberUser;
-				rsl = false;
-			}
+			size -= playerInput;
 		}
-		return rsl;
 	}
 }
