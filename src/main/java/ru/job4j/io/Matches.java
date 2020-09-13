07f20904d6c57083afa70bac72a42b4e2343837e
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-09-13 13:26:47 +0300

    2. Scanner и чтение числа из консоли. [#352426] - правки убрал дублирование кода и добавил валидацию

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,26 +3,32 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+	private static int size = 11;
+	private static boolean rsl = true;
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int size = 11;
-		while (true) {
-			System.out.println("Игрок 1 возьмите от 1 до 3 спичек");
-			int playerFirst = Integer.valueOf(input.nextLine());
-			size -= playerFirst;
+		while (rsl) {
+			if (!(takeUser(1, input))) break;
+			if (!(takeUser(2, input))) break;
+		}
+	}
+
+	private static boolean takeUser(int numberUser, Scanner input) {
+		System.out.println("Игрок " + numberUser + " возьмите от 1 до 3 спичек");
+		int player = Integer.valueOf(input.nextLine());
+		if (player > 3 || player < 0) {
+			System.out.println("Игрок " + numberUser
+					+ " взял не правильное количество спичек: " + player);
+			System.out.println("Возврат спичек и пропуск хода");
+		} else {
+			size -= player;
 			System.out.println("Осталось " + size + " спичек");
 			if (size == 0) {
-				System.out.println("Игрок 1 WIN!");
-				break;
-			}
-			System.out.println("Игрок 2 возьмите от 1 до 3 спичек");
-			int playerSecond = Integer.valueOf(input.nextLine());
-			size -= playerSecond;
-			System.out.println("Осталось " + size  + " спичек");
-			if (size == 0) {
-				System.out.println("Игрок 2 WIN!");
-				break;
+				System.out.println("Игрок " + numberUser + " WIN!");
+				rsl = false;
 			}
 		}
+		return rsl;
 	}
 }
