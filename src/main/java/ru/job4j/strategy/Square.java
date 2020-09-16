6a986d8c72c17e967dae42f1afc060c889180796
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-09-16 22:59:36 +0300

    7. Шаблон проектирования - Стратегия [#352439]

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,11 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+	@Override
+	public String draw() {
+		String ln = System.lineSeparator();
+		return "00000" + ln
+				+ "0   0" + ln
+				+ "00000";
+	}
+}
