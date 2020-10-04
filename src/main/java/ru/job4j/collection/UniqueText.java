026a8b823607874e97d433f8cea2543fd8d87c08
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-10-04 18:49:17 +0300

    2. Идентичные тексты [#352418]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+	public static boolean isEquals(String originText, String duplicateText) {
+		boolean rsl = true;
+		String[] origin = originText.split(" ");
+		String[] text = duplicateText.split(" ");
+		HashSet<String> check = new HashSet<>();
+		for (String value: origin) {
+			check.add(value);
+		}
+		if (origin.length == text.length) {
+			for (String value: text) {
+				if (!check.contains(value)) {
+					rsl = false;
+					break;
+				}
+			}
+		} else {
+			rsl = false;
+		}
+		return rsl;
+	}
+}
