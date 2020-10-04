b71bc5d9a547da10cf2a00f6ed443df7bab452d3
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-10-04 19:28:28 +0300

    4. final [#352465]

---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,16 @@
+package ru.job4j.ooa;
+
+public class Airport {
+	public static void main(String[] args) {
+		final Airbus airbus = new Airbus("A320");
+		System.out.println(airbus);
+		airbus.printModel();
+		airbus.printCountEngine();
+
+		final Airbus airbusTwo = new Airbus("A380");
+		System.out.println(airbus);
+
+		airbusTwo.setName("A380");
+		System.out.println(airbus);
+	}
+}
