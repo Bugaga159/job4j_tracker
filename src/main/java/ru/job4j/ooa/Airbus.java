db821c1a930dcbb78e6118879e003bbd626bcddc
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-10-05 08:28:05 +0300

    4. final [#352465] - правки

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -1,7 +1,7 @@
 package ru.job4j.ooa;
 
 public final class Airbus extends Aircraft {
-	private static int countEngine = 2;
+	private static final int COUNT_ENGINE = 2;
 
 	private String name;
 
@@ -23,8 +23,7 @@ public final class Airbus extends Aircraft {
 	}
 
 	public void printCountEngine() {
-		countEngine = 4;
-		System.out.println("Количество двигателей равно: " + countEngine);
+		System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
 	}
 
 	@Override
