547b4eb64a9242cf90608f54dc32c3c55253caca
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-10-06 10:10:18 +0300

    3. Банковские переводы [#352414] - правки

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -15,9 +15,8 @@ public class BankService {
 	public void addAccount(String passport, Account account) {
 		User user = findByPassport(passport);
 		if (user != null) {
-			Account res = findByRequisite(passport, account.getRequisite());
-			if (res == null) {
-				List<Account> accounts = users.get(user);
+			List<Account> accounts = users.get(user);
+			if (!accounts.contains(account)) {
 				accounts.add(account);
 				users.put(user, accounts);
 			}
@@ -47,19 +46,15 @@ public class BankService {
 								 String destPassport, String destRequisite,
 								 double amount) {
 		boolean rsl = false;
-		User userSrc = findByPassport(srcPassport);
-		User userDest = findByPassport(destPassport);
-		if (userSrc != null && userDest != null) {
-			Account accountSrc = findByRequisite(srcPassport, srcRequisite);
-			Account accountDest = findByRequisite(destPassport, destRequisite);
-			if (accountSrc != null
-				&& accountDest != null
-				&& accountSrc.getBalance() >= amount
-				) {
-				accountSrc.setBalance(accountSrc.getBalance() - amount);
-				accountDest.setBalance(accountDest.getBalance() + amount);
-				rsl = true;
-			}
+		Account accountSrc = findByRequisite(srcPassport, srcRequisite);
+		Account accountDest = findByRequisite(destPassport, destRequisite);
+		if (accountSrc != null
+			&& accountDest != null
+			&& accountSrc.getBalance() >= amount
+			) {
+			accountSrc.setBalance(accountSrc.getBalance() - amount);
+			accountDest.setBalance(accountDest.getBalance() + amount);
+			rsl = true;
 		}
 		return rsl;
 	}
