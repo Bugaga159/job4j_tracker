20e77a27695779b7bae52cc86d0bb148db73cc66
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-10-15 08:48:55 +0300

    2. Проверить две строчки на идентичность. - правки добавил новый метод

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -22,4 +22,34 @@ public class FreezeStr {
 		}
 		return rsl;
 	}
+
+	public static boolean eqWithHashMap(String left, String right) {
+		boolean rsl = false;
+		if (left.length() == right.length()) {
+			rsl = true;
+			Map<Character, Integer> map = new HashMap<>();
+			for (int i = 0; i < left.length(); i++) {
+				Character word = left.charAt(i);
+				if (!map.containsKey(word)) {
+					map.put(word, 1);
+				} else {
+					Integer value = map.get(word) + 1;
+					map.put(word, value);
+				}
+			}
+			for (int i = 0; i < right.length(); i++) {
+				Character word = right.charAt(i);
+				if (!map.containsKey(word)) {
+					rsl = false;
+					break;
+				}
+				if (map.get(word) == 0) {
+					rsl = false;
+					break;
+				}
+				map.put(word, map.get(word) - 1);
+			}
+		}
+		return rsl;
+	}
 }
--- src/test/java/ru/job4j/collection/FreezeStrTest.java
+++ src/test/java/ru/job4j/collection/FreezeStrTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.Matchers.is;
 import org.junit.Test;
 
 public class FreezeStrTest {
-
 	@Test
 	public void whenEq() {
 		assertThat(FreezeStr.eq("Hello", "Hlloe"), is(true));
@@ -20,5 +19,30 @@ public class FreezeStrTest {
 	@Test
 	public void whenNotMultiEq() {
 		assertThat(FreezeStr.eq("heloo", "hello"), is(false));
+	}
+
+	@Test
+	public void whenEmptyStringEq() {
+		assertThat(FreezeStr.eq("", "hello"), is(false));
+	}
+
+	@Test
+	public void wheneqWithHashMap() {
+		assertThat(FreezeStr.eqWithHashMap("Hello", "Hlloe"), is(true));
+	}
+
+	@Test
+	public void whenNoteqWithHashMap() {
+		assertThat(FreezeStr.eqWithHashMap("Hello", "Halle"), is(false));
+	}
+
+	@Test
+	public void whenNotMultieqWithHashMap() {
+		assertThat(FreezeStr.eqWithHashMap("heloo", "hello"), is(false));
+	}
+
+	@Test
+	public void whenEmptyStringeqWithHashMap() {
+		assertThat(FreezeStr.eqWithHashMap("", "hello"), is(false));
 	}
 }
