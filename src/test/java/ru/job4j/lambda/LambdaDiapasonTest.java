951c28459130b1a767f7b2b8bb7545a810389d95
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-11-02 23:20:24 +0300

    5. Подсчет функции в диапазоне. [#352225] - правка валидации

--- src/test/java/ru/job4j/lambda/LambdaDiapasonTest.java
+++ src/test/java/ru/job4j/lambda/LambdaDiapasonTest.java
@@ -25,8 +25,8 @@ public class LambdaDiapasonTest {
 
 	@Test
 	public void whenExponentiaFunctionExponentialResults() {
-		List<Double> result = LambdaDiapason.diapason(1, 3, x -> Math.pow(5,x) );
-		List<Double> expected = Arrays.asList(5D, 25D, 125D) ;
+		List<Double> result = LambdaDiapason.diapason(1, 3, x -> Math.pow(5, x));
+		List<Double> expected = Arrays.asList(5D, 25D, 125D);
 		assertThat(result, is(expected));
 	}
 
