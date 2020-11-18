a7fe90b203b21fda9bf0ea2ddc375339d4689a25
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-11-18 22:23:47 +0300

    3. Уникальность элементов и сортировка. [#352242] - правки добавил тест на метод

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+	public List<Profile> profilesList = new ArrayList<>();
+
+	@Before
+	public void setUp() {
+		profilesList.add(new Profile(new Address("Moscow", "Lenina", 12,2)));
+		profilesList.add(new Profile(new Address("Moscow", "Pushkina", 28,6)));
+		profilesList.add(new Profile(new Address("Sochi", "Lenina", 12,2)));
+		profilesList.add(new Profile(new Address("Moscow", "Lenina", 12,2)));
+		profilesList.add(new Profile(new Address("Moscow", "Chehova", 3,7)));
+		profilesList.add(new Profile(new Address("Kiev", "Lenina", 45,9)));
+	}
+
+	@Test
+	public void whenProfileToMapAddressListTest() {
+		Profiles profiles = new Profiles();
+		List<Address> rsl = profiles.collect(profilesList);
+		List<Address> expected = new ArrayList<>();
+		expected.add(new Address("Kiev", "Lenina", 45,9));
+		expected.add(new Address("Moscow", "Lenina", 12,2));
+		expected.add(new Address("Moscow", "Pushkina", 28,6));
+		expected.add(new Address("Moscow", "Chehova", 3,7));
+		expected.add(new Address("Sochi", "Lenina", 12,2));
+		assertThat(rsl, is(expected));
+	}
+}
