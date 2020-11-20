1e71c4d723d853222fb6a7196c53b4b5bf3aa97a
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-11-21 01:44:24 +0300

    2. Local-Variable Type Inference [#352252]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -24,8 +24,8 @@ public class PhoneDictionary {
 									.or(addressInclude)
 									.or(nameInclude)
 									.or(phoneInclude);
-		ArrayList<Person> result = new ArrayList<>();
-		for (Person person : persons) {
+		var result = new ArrayList<Person>();
+		for (var person : persons) {
 			if (combine.test(person)) {
 				result.add(person);
 			}
